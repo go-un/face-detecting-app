077cf023dbd825d01adfaa2499b71617a06a0699
go-un <guc931021@gmail.com>
2020-03-21 20:49:40 +0900

    Updates

---
+++ precache-manifest.25e18682d8d1aa3953a510f6d2f0e390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9693476efb687ccfbe918ad2f38fac51",
+    "url": "/face-detecting-app/index.html"
+  },
+  {
+    "revision": "a451b4b54ac647240364",
+    "url": "/face-detecting-app/static/css/main.190691b4.chunk.css"
+  },
+  {
+    "revision": "6486bc32581ca4833166",
+    "url": "/face-detecting-app/static/js/2.d7690b39.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/face-detecting-app/static/js/2.d7690b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a451b4b54ac647240364",
+    "url": "/face-detecting-app/static/js/main.3c759838.chunk.js"
+  },
+  {
+    "revision": "5d51395e474c948d7757",
+    "url": "/face-detecting-app/static/js/runtime-main.ec9f3a3f.js"
+  },
+  {
+    "revision": "f264fd6bc2d8c8a083517f403b80b4ab",
+    "url": "/face-detecting-app/static/media/chart.f264fd6b.svg"
+  }
+]);
